68f3a0d9133a26f58a4c2b11f02840297265e0b3
ShubhamGuptadln <sagarguptadln00@gmail.com>
2025-08-09 15:17:59 +0530

    completed all the work
    Signed-off-by: ShubhamGuptadln <sagarguptadln00@gmail.com>

---
+++ script.js
@@ -0,0 +1,79 @@
+let boxes=document.querySelectorAll(".box");
+let resetBtn=document.querySelector("#reset-btn");
+let turnO=true; //playerX, playerO
+
+const winPatterns=[
+    [0,1,2],
+    [0,3,6],
+    [0,4,8],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6],
+    [3,4,5],
+    [6,7,8],
+];
+
+boxes.forEach((box)=>{
+    box.addEventListener("click",()=>{
+       // console.log("box was clicked")
+        if(turnO){
+            box.innerText="O";
+            turnO=false;
+        }
+        else
+        {
+            box.innerText="X";
+            turnO=true;
+        }
+        box.disabled=true
+        chechkWinner();
+        isFilled();
+    })
+})
+
+const chechkWinner =()=>{
+    for(let pattern of winPatterns){
+        //console.log(boxes[pattern[0]],boxes[pattern[1]],boxes[pattern[2]]);
+        let pos1Val = boxes[pattern[0]].innerText;
+        let pos2Val = boxes[pattern[1]].innerText;
+        let pos3Val = boxes[pattern[2]].innerText;
+        if(pos1Val!=""&&pos2Val!=""&&pos3Val!="")
+        {
+            if(pos1Val===pos2Val && pos2Val===pos3Val)
+            {
+                console.log(`winner is ${pos1Val}`)
+                alert(`winner is ${pos1Val}`)
+                resetfunc();
+            }
+        }
+
+
+    }
+}
+
+let resetfunc=()=>{
+    console.log("reset button got clicked")
+    boxes.forEach((box)=>{
+        box.innerText="";
+        box.disabled=false;
+    })
+    turnO=true;
+}
+
+resetBtn.addEventListener("click",resetfunc)
+
+
+const isFilled=()=>{
+     let allFilled=true
+    boxes.forEach((box)=>{
+       
+        if(box.innerText==="")
+        {
+            allFilled=false;            
+        }           
+    })
+    if(allFilled){
+        alert("all boxes are filled");
+        resetfunc();
+    }
+}
